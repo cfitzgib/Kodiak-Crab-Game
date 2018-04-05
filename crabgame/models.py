3070e3cf03c280db37509d434aebdcf4ca585281
Hera Miao <heram@andrew.cmu.edu>
2018-04-05 16:28:19 -0400

    Created and added attributes to Crab, Images, Oocytes and Session model

--- crabgame/models.py
+++ crabgame/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+class Crab(models.Model):
+    done_oocytes = models.IntegerField(validators = [MaxValueValidator(10)])
+
+class Images(models.Model):
+    crab = models.ForeignKey(Crab, on_delete = models.CASCADE)
+
+class Oocytes(models.Model):
+    crab = models.ForeignKey(Crab, on_delete = models.CASCADE)
+    image = models.ForeignKey(Images, on_delete = models.CASCADE)
+
 class PlaySession(models.Model):
-    num_photos = models.IntegerField(default = 5)
+    num_photos = models.IntegerField(default = 5) # assume 5 photos per game session for now
