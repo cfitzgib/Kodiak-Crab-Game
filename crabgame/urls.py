f61fea4aabb240f9f7ae31e6ae9a446e108c0552
Hera Miao <heram@andrew.cmu.edu>
2018-04-14 16:25:17 -0400

    set up Django templates for views

--- crabgame/urls.py
+++ crabgame/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:image_id>/', views.playImg, name='playImg'),
 ]
--- crabgame/views.py
+++ crabgame/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files import File
+import os
+from .models import Crab, Image, Oocyte, Intermediate, PlaySession, SchoolClass
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Crab Game index.")
+
+def playImg(request, image_id):
+    displayImg = Image.objects.get(pk=4)
+    content = displayImg.read()
+    return HttpResponse(content)
+    return HttpResponse("You're looking at image # %s" % image_id)
