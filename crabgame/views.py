a582762eeab7df916de8e166291382fca5278b58
Hera Miao <heram@andrew.cmu.edu>
2018-04-17 16:21:32 -0400

    randomized displayed image on screen

--- crabgame/views.py
+++ crabgame/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Crab, Image, Oocyte, Intermediate, PlaySession, SchoolClass
+import random
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the Crab Game index.")
 
 def playCrabImg(request, crab_sample):
-    getCrab = Crab.objects.get(sample_num = crab_sample)
-    crabImgs = getCrab.image_set.all()
-    displayImg = crabImgs[0] # eventually this will be a random number within the size of the set (crabImgs.count())
+    getCrab = Crab.objects.get(sample_num = crab_sample) # get a crab with a specified sample num
+    crabImgs = getCrab.image_set.all() # get all the image instances for that specific crab
+    displayImg = random.choice(crabImgs) # choose a random image instance to display from that crab
     return render(request, 'crabgame/playCrabImg.html', {'displayImg': displayImg})
